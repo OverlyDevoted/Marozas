78c26bbc0f1d1681c0ca7c289546f6be2bcf1de6
OverlyDevoted <58229165+OverlyDevoted@users.noreply.github.com>
2022-03-17 15:23:35 +0200

    Added player deletion and game deletion upon connection lost with a user added ready functionality

--- server.js
+++ server.js
@@ -22,14 +22,96 @@ wsServer.on("request", request =>{
     const connection = request.accept(null, request.origin)
     connection.on("open", () => console.log("opened"))
     connection.on("close", (sender, e) => {
-
+        let game = clients[clientID].game
+        if(game)
+        {
+            console.log(game);
+            RemoveFromGame(clientID);
+        }
+        RemoveClient(clientID);
     })
     connection.on("message", message=>{
-        
-        const result = JSON.parse(message.utf8Data)
+        let result = {};
+        try {
+            result = JSON.parse(message.utf8Data)
+        } catch (error) {
+            console.log(error)
+            return;
+        }
         //receiving message from user
         console.log(result)
+        switch(result.method)
+        {
+            case "create": {
+                const gameID = createGuid();
+                games[gameID] = {"clients":[]};
+                SendMessage(clients[clientID].connection, "create", {"gameID":gameID});
+            } break;
+            case "join":
+                const gameID = result.gameID;
+                const game = games[gameID];
+                if(!game)
+                {
+                    console.log("no such game");
+                    SendMessage(clients[clientID].connection, "error", {"message":"Game " + gameID + " does not exist"});
+                    return;
+                }
+                if(game.clients.length > 4)
+                {
+                    console.log("lobby full");
+                    SendMessage(clients[clientID].connection, "error", {"message":"Game " + gameID + " is full"});
+                    return;
+                }
+                clients[clientID].game = gameID;
+                game.clients.push({
+                    "clientID":clientID,
+                    "prio": game.clients.length,
+                    "ready": "false"
+                })
+                games[gameID] = game;
+                game.clients.forEach(c =>{
+                    if(c.clientID === clientID)
+                    {
+                        game.gameID = gameID;
+                        SendMessage(clients[c.clientID].connection,"join", game);
+                    }
+                    else
+                    {
+                        SendMessage(clients[c.clientID].connection,"playerJoined", {"clientID":clientID});
+                    }
+                });
 
+                break;
+            case "disconnect":
+                {
+                    RemoveFromGame(clientID);
+                }
+                break;
+            case "ready":
+                {
+                    const gameID = clients[clientID].game;
+                    let game = games[gameID];
+                    
+                    game.clients.forEach(c =>{
+                        if(c.clientID == clientID)
+                        {
+                            if(c.ready)
+                                c.ready = false;
+                            else
+                                c.ready = true;
+                        }
+                        else
+                        {
+                            SendMessage(clients[c.clientID].connection, "ready", {"clientID":clientID});
+                        }
+                    });
+                    games[gameID] = game;
+                }
+                break;
+            default:
+                console.log("No such function");
+                break;
+        }
     })
     
     const clientID = createGuid();
@@ -40,11 +122,58 @@ wsServer.on("request", request =>{
     let method = {
         "method":"connect"
     };
-    console.log(connection.state);
     SendMessage(connection, "connect", {"clientID":clientID});
-    console.log(clientID);
 })
-
+function RemoveFromGame(clientID)
+{
+    const gameID = clients[clientID].game;
+    clients[clientID].game = null;
+    let newClients = [];
+    let prio;
+    const game = games[gameID];
+    if(game.clients.length === 1)
+    {
+        RemoveGame(gameID);
+        return;
+    }
+    //find prio of the disconnected player
+    game.clients.forEach(c=>{
+        if(c.clientID === clientID)
+        {
+            prio = c.prio;
+        }
+    });
+    
+    game.clients.forEach(c=>{
+        //if not the disconnected player
+        if(c.clientID != clientID)
+        {
+            //new prio = current prio
+            newPrio = c.prio;
+            //if the old prio greater than 
+            if(c.prio > prio)
+                newPrio--;
+            c.prio = newPrio;
+            newClients.push(c);
+        }
+    });
+    game.clients = newClients;
+    games[gameID] = game;
+    console.log(games[gameID]);
+    game.clients.forEach(c =>{
+        SendMessage(clients[c.clientID].connection,"playerDisconnected", {"clientID":clientID, "prio":c.prio});
+    });
+}
+function RemoveGame(gameID)
+{
+    delete games[gameID];
+    console.log("Game " + gameID + " has been deleted");
+}
+function RemoveClient(clientID)
+{
+    delete clients[clientID];
+    console.log("Player " + clientID + " has disconnected");
+}
 function createGuid(){  
     function S4() {  
        return (((1+Math.random())*0x10000)|0).toString(16).substring(1);  
